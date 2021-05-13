d591f36ea0c3b17828c671b32aeadd40e2237e5a
mathamp <jwzz123123@gmail.com>
2021-05-13 23:47:48 +0900

    style : Split Team4Header.h into RemoveUnused.h and Team4Header.h

--- src/core/Team4Header.h
+++ src/core/Team4Header.h
@@ -1,27 +1,6 @@
 #ifndef TEAM4HEADER_H
 #define TEAM4HEADER_H
 
-#include "llvm/IR/PassManager.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-
-using namespace llvm;
-using namespace std;
-
-#include <vector>
-
-
-class RemoveUnusedPass : public llvm::PassInfoMixin<RemoveUnusedPass> {
-private:
-  template <typename T> void sortDominated(vector<T> &vec, DominatorTree &DT);
-  void eraseInstructionsRecursive(Instruction *inst);
-public:
-  vector<BasicBlock*> getUnreachableBB(Function &F, FunctionAnalysisManager &FAM);
-  void eraseUnreachableBB(Function &F, FunctionAnalysisManager &FAM);
-  void eraseUnusedInstruction(Function &F, FunctionAnalysisManager &FAM);
-  void eraseUnloadedAlloca(Function &F, FunctionAnalysisManager &FAM);
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
-};
-
+#include "RemoveUnused.h"
 
 #endif
