3eac78b13c97a4c6e75b2b53a91416cf4faadaeb
cs71107 <psjlds@gmail.com>
2021-05-13 23:53:03 +0900

    And optimization update
    add (2^C)-1 X format update.

--- src/core/ArithmeticPass.cpp
+++ src/core/ArithmeticPass.cpp
@@ -10,57 +10,52 @@ PreservedAnalyses ArithmeticPass::run(Function &F, FunctionAnalysisManager &FAM)
   for(BasicBlock &BB: F){
      
     //vectors for store change instructions.
-    vector<Instruction *> InstAdd,InstShl,InstLShr,InstAShr,InstAnd,InstAndMx;
+    vector<Instruction *> InstAdd, InstShl, InstLShr, InstAShr, InstAnd, InstAndMx;
 
     for(Instruction &I : BB){
             
       //Case one: add X X -> mul X 2
-      if(match(&I,m_Add(m_Value(FirstVal),m_Deferred(FirstVal)))){
+      if(match(&I, m_Add(m_Value(FirstVal), m_Deferred(FirstVal)))){
         InstAdd.push_back(&I);
       }
 
       //Case two: shl X C -> mul X (2^C)
-      if(match(&I, m_Shl(m_Value(FirstVal),m_ConstantInt(ConsVal)))){
+      if(match(&I, m_Shl(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
         InstShl.push_back(&I);
       }
 
       //Case three: lshr X C -> Udiv X (2^C)
-      if(match(&I, m_LShr(m_Value(FirstVal),m_ConstantInt(ConsVal)))){
+      if(match(&I, m_LShr(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
         InstLShr.push_back(&I);
       }
 
       //Case four: ashr X C -> Sdiv X (2^C)
-      if(match(&I, m_AShr(m_Value(FirstVal),m_ConstantInt(ConsVal)))){
+      if(match(&I, m_AShr(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
         InstAShr.push_back(&I);
       }
 
       //Case five: and X (2^C-1) -> udiv X (2^C)
-      if(match(&I, m_And(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
-        uint64_t cons = ConsVal->getZExtValue(),bi = 0;
+      if(match(&I, m_And(m_Value(FirstVal), m_ConstantInt(ConsVal))) || match(&I, m_And(m_ConstantInt(ConsVal), m_Value(FirstVal)))){
+        
+        uint64_t cons = ConsVal->getZExtValue(), width = FirstVal->getType()->getIntegerBitWidth();
 
-        bool chk = false;
-
-        //check cons is 2^C-1 form.
-        for(unsigned i=0;i<63;i++){
-          bi = (bi<<1)|1;
-          if(bi==cons){
-            chk = true;
-            break;
-          }
-        }
-                
-        if(chk){
-          InstAnd.push_back(&I);
-        } else if(((bi<<1)|1)==cons){//if cons is unsigned max, push it InstAndMx
+        if(cons==UINT64_MAX){
           InstAndMx.push_back(&I);
+        } else if(width==64){
+          InstAnd.push_back(&I);
+        } else if((cons+1) == (1ull<<width)){
+          InstAndMx.push_back(&I);
+        } else if (cons && (! (cons & (cons+1)))) {
+          InstAnd.push_back(&I);
         }
       }
+
     }
 
     //Change Instruction to mul x 2
     for(Instruction *AddI : InstAdd){
       Value* FirstOperand = AddI->getOperand(0);
-      Instruction*  NewInst = BinaryOperator::Create(Instruction::Mul,FirstOperand,ConstantInt::get(FirstOperand->getType(),2));
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::Mul, FirstOperand, ConstantInt::get(FirstOperand->getType(),2));
       ReplaceInstWithInst(AddI,NewInst);
     }
 
@@ -69,7 +64,7 @@ PreservedAnalyses ArithmeticPass::run(Function &F, FunctionAnalysisManager &FAM)
       Value* FirstOperand = ShlI->getOperand(0);
       ConstantInt* ShlVal = dyn_cast<ConstantInt> (ShlI->getOperand(1));
       uint32_t ushlval = ShlVal->getZExtValue();
-      Instruction*  NewInst = BinaryOperator::Create(Instruction::Mul,FirstOperand,ConstantInt::get(FirstOperand->getType(),(1ull<<ushlval)));
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::Mul, FirstOperand, ConstantInt::get(FirstOperand->getType(),(1ull<<ushlval)));
       ReplaceInstWithInst(ShlI,NewInst);
     }
 
@@ -78,7 +73,7 @@ PreservedAnalyses ArithmeticPass::run(Function &F, FunctionAnalysisManager &FAM)
       Value* FirstOperand = LShrI->getOperand(0);
       ConstantInt* LShrVal = dyn_cast<ConstantInt> (LShrI->getOperand(1));
       uint32_t ushrval = LShrVal->getZExtValue();
-      Instruction*  NewInst = BinaryOperator::Create(Instruction::UDiv,FirstOperand,ConstantInt::get(FirstOperand->getType(),(1ull<<ushrval)));
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::UDiv, FirstOperand,ConstantInt::get(FirstOperand->getType(),(1ull<<ushrval)));
       ReplaceInstWithInst(LShrI,NewInst);
     }
 
@@ -87,16 +82,24 @@ PreservedAnalyses ArithmeticPass::run(Function &F, FunctionAnalysisManager &FAM)
       Value* FirstOperand = AShrI->getOperand(0);
       ConstantInt* AShrVal = dyn_cast<ConstantInt> (AShrI->getOperand(1));
       uint32_t ashrval = AShrVal->getZExtValue();
-      Instruction*  NewInst = BinaryOperator::Create(Instruction::SDiv,FirstOperand,ConstantInt::get(FirstOperand->getType(),(1ull<<ashrval)));
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::SDiv, FirstOperand, ConstantInt::get(FirstOperand->getType(),(1ull<<ashrval)));
       ReplaceInstWithInst(AShrI,NewInst);
     }
 
     //Change Instruction to urem x (1<<c)
     for(Instruction *AndI : InstAnd){
       Value* FirstOperand = AndI->getOperand(0);
-      ConstantInt* AndVal = dyn_cast<ConstantInt> (AndI->getOperand(1));
+      Value* SecondOperand = AndI->getOperand(1);
+      Value* XOperand;
+      ConstantInt* AndVal = dyn_cast<ConstantInt> (FirstOperand);
+      if(AndVal==NULL){
+        AndVal = dyn_cast<ConstantInt> (SecondOperand);
+        XOperand = FirstOperand;
+      } else {
+        XOperand = SecondOperand;
+      }
       uint64_t andval = AndVal->getZExtValue();
-      Instruction* NewInst = BinaryOperator::Create(Instruction::URem,FirstOperand,ConstantInt::get(FirstOperand->getType(),(andval+1)));
+      Instruction* NewInst = BinaryOperator::Create(Instruction::URem, XOperand, ConstantInt::get(XOperand->getType(),(andval+1)));
       ReplaceInstWithInst(AndI,NewInst);
     }
 
@@ -106,7 +109,6 @@ PreservedAnalyses ArithmeticPass::run(Function &F, FunctionAnalysisManager &FAM)
       AndMxI->replaceAllUsesWith(FirstOperand);
       InstRemove.push_back(AndMxI);
     }
-
   }
   
   //Remove Instructions from Parent.
