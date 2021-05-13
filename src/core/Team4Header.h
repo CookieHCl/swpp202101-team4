d55450ec3fd567a98b5b0d0198035fc0957c2bbf
cs71107 <psjlds@gmail.com>
2021-05-14 00:00:59 +0900

    header file fixing
    adding Arithmeticpass.h for Arithmeticpass.cpp, simply Team4header.h

--- src/core/Team4Header.h
+++ src/core/Team4Header.h
@@ -1,25 +1,6 @@
 #ifndef TEAM4HEADER_H
 #define TEAM4HEADER_H
 
-#include "llvm/IR/PassManager.h"
-#include "llvm/Passes/PassBuilder.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/Instructions.h"
-#include "llvm/IR/InstrTypes.h"
-#include "llvm/IR/Type.h"
-#include "llvm/IR/PatternMatch.h"
-#include "llvm/Transforms/Utils/BasicBlockUtils.h"
-
-#include <cassert>
-#include <vector>
-using namespace llvm;
-using namespace std;
-using namespace llvm::PatternMatch;
-
-class ArithmeticPass : public PassInfoMixin<ArithmeticPass> {
-public:
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
-};
+#include "Arithmeticpass.h"
 
 #endif
