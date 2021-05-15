34ba098309f980d127c74da8f6aadc906ede4106
pasa3232 <68896140+pasa3232@users.noreply.github.com>
2021-05-15 14:37:07 +0900

    Changes added after code review (3)

--- src/core/functionInlinePass.cpp
+++ src/core/functionInlinePass.cpp
@@ -23,18 +23,30 @@ PreservedAnalyses FunctionInlinePass::run(Module &M, ModuleAnalysisManager &MAM)
   RegisterGraph tRG(M);
   RegisterGraph *RG = &tRG;
 
-  // caller to inline
-  vector<CallBase *> do_inline;
+  // caller to inline and Function's numColor
+  vector<pair<CallBase *, unsigned> > do_inline;
+  map<Function *, unsigned> numColors;
 
   for(Function& F : M) {
+
+    if(F.isDeclaration()) continue;
+    // The number of registers can be found through
+    // backend::RegisterGraph.getNumColors(Function*)
+    numColors[&F] = RG->getNumColors(&F);
+  }
+
+  for(Function& F : M) {
+    
     if(F.isDeclaration()) continue;
     
     // If there is no extra register, do nothing for the function.
 
-    // The number of registers can be found through
-    // backend::RegisterGraph.getNumColors(Function*)
-    unsigned numColor = RG->getNumColors(&F);
+    unsigned numColor = numColors[&F];
     if(numColor >= REGISTER_CAP) continue;
+
+    // Where F's callers start to be stored in do_inline
+    int st = do_inline.size();
+
     for(BasicBlock& BB : F) {
       for(Instruction& I : BB) {
 
@@ -42,24 +54,39 @@ PreservedAnalyses FunctionInlinePass::run(Module &M, ModuleAnalysisManager &MAM)
         auto *caller = dyn_cast<CallInst>(&I);
         if(!caller) continue;
         
-        // If the sum of colors exceeds the REGISTER_CAP by inlining, do not inlining
-        Function *callee = caller->getCalledFunction();
-        if(!callee) continue;
-        unsigned f_numColor = RG->getNumColors(callee);
-        if(numColor + f_numColor > REGISTER_CAP) continue;
-
         // Minimal filter to detect invalid constructs for inlining
+        Function *callee = caller->getCalledFunction();
+        if(!callee || callee->isDeclaration()) continue;
         if(!(isInlineViable(*callee).isSuccess())) continue;
 
-        numColor += f_numColor;
-        do_inline.push_back(caller);
+        // If the sum of colors exceeds the REGISTER_CAP by inlining, do not inlining
+        unsigned f_numColor = numColors[callee];
+        if(numColor + f_numColor > REGISTER_CAP) {
+          // An index that element have maximum register count
+          unsigned i_m = -1;
+
+          for(int i = st; i < do_inline.size(); ++i) {
+            if(i_m == -1) i_m = i;
+            else if(do_inline[i_m].second < do_inline[i].second) i_m = st;
+          }
+          if(i_m != -1 && f_numColor < do_inline[i_m].second) {
+            numColor += f_numColor - do_inline[i_m].second;
+            do_inline[i_m] = make_pair(caller, f_numColor);
+          }
+        }
+        else {
+          numColor += f_numColor;
+          do_inline.push_back(make_pair(caller, f_numColor));
+        }
       }
     }
+    // update numColors
+    numColors[&F] = numColors;
   }
 
   for(CallBase *caller : do_inline) {
 
-    // Inline caller with not insert Lifetime
+    // Inline caller without insert Lifetime
 
     // llvm::InlineFunctionInfo.InlineFunction(CallBase &CB, InlineFunctionInfo &IFI,
     //                                         AAResults *CalleeAAR,
