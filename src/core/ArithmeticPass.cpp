2603a9d2619829f391b3bd4320a8848864fb37d9
cs71107 <69337848+cs71107@users.noreply.github.com>
2021-05-15 19:19:46 +0900

    [Sprint 1]Arithmetic pass (#8)
    * Arithmeticpass Implementation
    
    create Arithmeticpass.cpp and modyfying main.cpp and Team4Header.h for correct arithmetic optimization.
    
    * adding test files
    
    add test Arithmeticpass-test1,Arithmeticpass-test2,Arithmeticpass-test3 for testing Arithmeticpass optimization.
    this cases generated by translation .c files.
    
    * ArithmeticPass overflow fix
    
    fix for prevent overflow in shl,lshr,ashr type
    
    * And optimization update
    
    add (2^C)-1 X format update.
    
    * header file fixing
    
    adding Arithmeticpass.h for Arithmeticpass.cpp, simply Team4header.h
    
    * ArithmeticPass and max_value update
    
    iteration is modified. now use iterator instremove vector is removed.
    
    * ArithmeticPass fix to prevent iterator crash
    
    add iterator variable tmp_itr to point current instruction, this may prevent crashing.

---
+++ src/core/ArithmeticPass.cpp
@@ -0,0 +1,109 @@
+#include "Team4Header.h"
+
+PreservedAnalyses ArithmeticPass::run(Function &F, FunctionAnalysisManager &FAM) {
+
+  Value* FirstVal; //FirstOperand Value
+  ConstantInt* ConsVal; //Constant Value
+
+  for(BasicBlock &BB: F){
+
+    //vectors for store change instructions.
+    vector<Instruction *> InstAdd, InstShl, InstLShr, InstAShr, InstAnd;
+
+    for(auto itr = BB.begin(), en = BB.end(); itr!=en;){
+
+      auto tmp_itr = itr++;
+      Instruction &I = *tmp_itr;
+
+      //Case one: add X X -> mul X 2
+      if(match(&I, m_Add(m_Value(FirstVal), m_Deferred(FirstVal)))){
+        InstAdd.push_back(&I);
+      }
+
+      //Case two: shl X C -> mul X (2^C)
+      if(match(&I, m_Shl(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
+        InstShl.push_back(&I);
+      }
+
+      //Case three: lshr X C -> Udiv X (2^C)
+      if(match(&I, m_LShr(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
+        InstLShr.push_back(&I);
+      }
+
+      //Case four: ashr X C -> Sdiv X (2^C)
+      if(match(&I, m_AShr(m_Value(FirstVal), m_ConstantInt(ConsVal)))){
+        InstAShr.push_back(&I);
+      }
+
+      //Case five: and X (2^C-1) -> udiv X (2^C)
+      if(match(&I, m_And(m_Value(FirstVal), m_ConstantInt(ConsVal))) || match(&I, m_And(m_ConstantInt(ConsVal), m_Value(FirstVal)))){
+
+        uint64_t cons = ConsVal->getZExtValue(), width = FirstVal->getType()->getIntegerBitWidth();
+
+        if(cons==UINT64_MAX){
+          ReplaceInstWithValue(BB.getInstList(),tmp_itr,FirstVal);
+        } else if(width==64){
+          InstAnd.push_back(&I);
+        } else if((cons+1) == (1ull<<width)){
+          ReplaceInstWithValue(BB.getInstList(),tmp_itr,FirstVal);
+        } else if (cons && (! (cons & (cons+1)))) {
+          InstAnd.push_back(&I);
+        }
+      }
+
+    }
+
+    //Change Instruction to mul x 2
+    for(Instruction *AddI : InstAdd){
+      Value* FirstOperand = AddI->getOperand(0);
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::Mul, FirstOperand, ConstantInt::get(FirstOperand->getType(),2));
+      ReplaceInstWithInst(AddI, NewInst);
+    }
+
+    //Change Instruction to mul x (1<<c)
+    for(Instruction *ShlI : InstShl){
+      Value* FirstOperand = ShlI->getOperand(0);
+      ConstantInt* ShlVal = dyn_cast<ConstantInt> (ShlI->getOperand(1));
+      uint32_t ushlval = ShlVal->getZExtValue();
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::Mul, FirstOperand, ConstantInt::get(FirstOperand->getType(),(1ull<<ushlval)));
+      ReplaceInstWithInst(ShlI, NewInst);
+    }
+
+    //Change Instruction to udiv x (1<<c)
+    for(Instruction *LShrI : InstLShr){
+      Value* FirstOperand = LShrI->getOperand(0);
+      ConstantInt* LShrVal = dyn_cast<ConstantInt> (LShrI->getOperand(1));
+      uint32_t ushrval = LShrVal->getZExtValue();
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::UDiv, FirstOperand,ConstantInt::get(FirstOperand->getType(),(1ull<<ushrval)));
+      ReplaceInstWithInst(LShrI, NewInst);
+    }
+
+    //Change Instruction to sdiv x (1<<c)
+    for(Instruction *AShrI : InstAShr){
+      Value* FirstOperand = AShrI->getOperand(0);
+      ConstantInt* AShrVal = dyn_cast<ConstantInt> (AShrI->getOperand(1));
+      uint32_t ashrval = AShrVal->getZExtValue();
+      Instruction*  NewInst = BinaryOperator::Create(Instruction::SDiv, FirstOperand, ConstantInt::get(FirstOperand->getType(),(1ull<<ashrval)));
+      ReplaceInstWithInst(AShrI, NewInst);
+    }
+
+    //Change Instruction to urem x (1<<c)
+    for(Instruction *AndI : InstAnd){
+      Value* FirstOperand = AndI->getOperand(0);
+      Value* SecondOperand = AndI->getOperand(1);
+      Value* XOperand;
+      ConstantInt* AndVal = dyn_cast<ConstantInt> (FirstOperand);
+      if(AndVal==NULL){
+        AndVal = dyn_cast<ConstantInt> (SecondOperand);
+        XOperand = FirstOperand;
+      } else {
+        XOperand = SecondOperand;
+      }
+      uint64_t andval = AndVal->getZExtValue();
+      Instruction* NewInst = BinaryOperator::Create(Instruction::URem, XOperand, ConstantInt::get(XOperand->getType(),(andval+1)));
+      ReplaceInstWithInst(AndI, NewInst);
+    }
+  }
+
+  return PreservedAnalyses::all();
+}
--- src/core/Team4Header.h
+++ src/core/Team4Header.h
@@ -1,6 +1,7 @@
 #ifndef TEAM4HEADER_H
 #define TEAM4HEADER_H
 
+#include "Arithmeticpass.h"
 #include "functionInlinePass.h"
 #include "RemoveUnused.h"
 
