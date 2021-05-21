284da5b3906121ed65ebd55362a6b6c960015ed2
CookieHCl <nhc7502@snu.ac.kr>
2021-05-21 19:56:13 +0900

    Add compiler option for pass selection

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -6,12 +6,12 @@
 #include "../backend/UnfoldVectorInst.h"
 #include "../backend/SplitSelfLoop.h"
 
+#include "FixedCommandLine.h"
 #include "Team4Header.h"
 
 #include "llvm/AsmParser/Parser.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/raw_os_ostream.h"
-#include "llvm/Support/CommandLine.h"
 #include "llvm/IR/PassManager.h"
 
 #include <string>
@@ -36,9 +36,37 @@ static cl::alias optPrintProgressA(
     "v", cl::desc("Alias for --verbose"),
     cl::aliasopt(optPrintProgress), cl::cat(optCategory));
 
+enum struct Opts {
+  Arithmetic,
+  FunctionInline,
+  RemoveUnused,
+  SimplifyCFG
+};
+
+static unsigned optimizationBits;
+static cl::bits<Opts, unsigned> optimizations(
+    "passes", cl::desc("Apply only selected optimizations:"),
+    cl::location(optimizationBits), cl::CommaSeparated, cl::cat(optCategory),
+    cl::values(
+      clEnumValN(Opts::Arithmetic, "arith",
+        "Replace arithmetic operations to cheaper operations"),
+      clEnumValN(Opts::FunctionInline, "inline",
+        "Inline functions if possible"),
+      clEnumValN(Opts::RemoveUnused, "unused",
+        "Remove unused BB & alloca & instruction"),
+      clEnumValN(Opts::SimplifyCFG, "simplifycfg",
+        "Simplify and canonicalize the CFG")));
+
+#define IFSET(enum, X) if (optimizations.isSet(enum)) { X; }
+
 int main(int argc, char *argv[]) {
   //Parse command line arguments
+  cl::HideUnrelatedOptions(optCategory);
   cl::ParseCommandLineOptions(argc, argv);
+  if (!optimizationBits) {
+    // if optimization is not specified, all optimizations should be enabled
+    optimizationBits = -1;
+  }
 
   //Parse input LLVM IR module
   LLVMContext Context;
@@ -74,17 +102,17 @@ int main(int argc, char *argv[]) {
   PB.crossRegisterProxies(LAM, FAM, CGAM, MAM);
 
   // Add existing IR passes
-  FPM.addPass(SimplifyCFGPass());
+  IFSET(Opts::SimplifyCFG, FPM.addPass(SimplifyCFGPass()))
 
   // Add IR passes
-  FPM.addPass(ArithmeticPass());
-  FPM.addPass(RemoveUnusedPass());
+  IFSET(Opts::Arithmetic, FPM.addPass(ArithmeticPass()))
+  IFSET(Opts::RemoveUnused, FPM.addPass(RemoveUnusedPass()))
 
   // Add existing IR passes
-  FPM.addPass(SimplifyCFGPass());
+  IFSET(Opts::SimplifyCFG, FPM.addPass(SimplifyCFGPass()))
 
   // Execute IR passes
-  MPM.addPass(FunctionInlinePass());
+  IFSET(Opts::FunctionInline, MPM.addPass(FunctionInlinePass()))
   MPM.addPass(createModuleToFunctionPassAdaptor(std::move(FPM)));
   MPM.run(*M, MAM);
 
