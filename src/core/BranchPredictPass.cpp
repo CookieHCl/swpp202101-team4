25cda107fb8f056c5e1509c53bb26f0ab452bda4
CookieHCl <nhc7502@snu.ac.kr>
2021-05-23 21:56:45 +0900

    Optimize recursive function

--- src/core/BranchPredictPass.cpp
+++ src/core/BranchPredictPass.cpp
@@ -3,6 +3,19 @@
 PreservedAnalyses BranchPredictPass::run(Function &F, FunctionAnalysisManager &FAM) {
   printDebug("---------- Start BranchPredictPass ----------");
 
+  // set of BB that has recursive call
+  SmallPtrSet<BasicBlock*, 4> RecursiveBlocks;
+  for (BasicBlock &BB : F) {
+    for (Instruction &I : BB) {
+      if (auto *CB = dyn_cast<CallBase>(&I)) {
+        if (CB->getCalledFunction() == &F) {
+          RecursiveBlocks.insert(&BB);
+          break;
+        }
+      }
+    }
+  }
+
   // branch probability info to check branch probability
   BranchProbabilityInfo &BPI = FAM.getResult<BranchProbabilityAnalysis>(F);
 
@@ -24,12 +37,38 @@ PreservedAnalyses BranchPredictPass::run(Function &F, FunctionAnalysisManager &F
     const BranchProbability &TrueBBProb = BPI.getEdgeProbability(&BB, TrueBB);
     const BranchProbability &FalseBBProb = BPI.getEdgeProbability(&BB, FalseBB);
 
-    printDebug("Found br: ", *Terminator);
-    printDebug("True br probability: ", TrueBBProb);
-    printDebug("False br probability: ", FalseBBProb);
+    // check if blocks has recursive call
+    bool isTrueBBRecursive = RecursiveBlocks.contains(TrueBB);
+    bool isFalseBBRecursive = RecursiveBlocks.contains(FalseBB);
 
-    // false branch should be more likely than true branch
-    if (TrueBBProb > FalseBBProb) {
+    printDebug("Found br: ", *Terminator);
+    printDebug("True br ",
+      isTrueBBRecursive ? " is recursive, " : "is not recursive, ",
+      "probability: ", TrueBBProb);
+    printDebug("False br ",
+      isFalseBBRecursive ? " is recursive, " : "is not recursive, ",
+      "probability: ", FalseBBProb);
+
+    // if true branch is more likely than false branch, swap
+    bool shouldSwap = TrueBBProb > FalseBBProb;
+    if (isTrueBBRecursive ^ isFalseBBRecursive && // check if only one is recursive
+      isTrueBBRecursive ^ shouldSwap) { // check if result agree with probability
+      // only TrueBB is recursive & TrueBBProb <= FalseBBProb
+      // or only FalseBB is recursive & TrueBBProb > FalseBBProb
+      // contradiction!
+
+      // heuristic: use recursive if max prob < 0.9, use prob if max prob >= 0.9
+      // use floating point to avoid overflow
+      bool isProbUnreliable = std::max(TrueBBProb, FalseBBProb).getNumerator()
+        < 0.9 * BranchProbability::getDenominator();
+
+      printDebug("Contradiction; Use ", isProbUnreliable ? "recursive" : "prob");
+      if (isProbUnreliable) {
+        shouldSwap = !shouldSwap;
+      }
+    }
+
+    if (shouldSwap) {
       printDebug("Swapping branch..");
 
       if (auto *CmpCond = dyn_cast<CmpInst>(Cond)) {
