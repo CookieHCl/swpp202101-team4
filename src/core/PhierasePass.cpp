698416fb258df8f32495c7d13e1f6a1687220709
cs71107 <psjlds@gmail.com>
2021-05-29 16:57:12 +0900

    adding comment & fixing minor syntax

--- src/core/PhierasePass.cpp
+++ src/core/PhierasePass.cpp
@@ -4,7 +4,7 @@ PreservedAnalyses PhierasePass::run(Function &F, FunctionAnalysisManager &FAM){
   
   queue<BasicBlock*> que;
 
-  for(auto B_itr = F.begin(),B_en = F.end(); B_itr!=B_en; ) {//initialize que.
+  for (auto B_itr = F.begin(),B_en = F.end(); B_itr != B_en; ) {//initialize que.
 
     auto tmp_B_itr = B_itr++;
     BasicBlock &BB = (*tmp_B_itr);
@@ -19,12 +19,12 @@ PreservedAnalyses PhierasePass::run(Function &F, FunctionAnalysisManager &FAM){
     que.push(&BB);
   }
 
-  while(!que.empty()){//iterate blocks using queue.
+  while (!que.empty()) {//iterate blocks using queue.
 
     BasicBlock *Cur_BB = que.front();
     que.pop();
 
-    for(auto pre_itr = pred_begin(Cur_BB), pre_en = pred_end(Cur_BB); pre_itr!=pre_en;) {
+    for (auto pre_itr = pred_begin(Cur_BB), pre_en = pred_end(Cur_BB); pre_itr!=pre_en;) {
 
       auto tmp_pre_itr = pre_itr++;
       BasicBlock *pre_BB = (*tmp_pre_itr);
@@ -33,13 +33,13 @@ PreservedAnalyses PhierasePass::run(Function &F, FunctionAnalysisManager &FAM){
 
       BasicBlock* Cloned_BB = CloneBasicBlock(Cur_BB,VM); //cloning basicblock.
 
-      for(auto itr = Cloned_BB->begin(), e = Cloned_BB->end(); itr!=e;){
+      for(auto itr = Cloned_BB->begin(), e = Cloned_BB->end(); itr != e;){
         auto tmp_itr = itr++;
         Instruction *I = &(*tmp_itr);
         PHINode *Phi = dyn_cast<PHINode> (I);
 
         if(Phi) {
-          for(unsigned int i=0; i<Phi->getNumOperands(); ++i){
+          for (unsigned int i = 0; i < Phi->getNumOperands(); ++i) {
 
             BasicBlock *Cur_pre_BB = Phi->getIncomingBlock(i);
             if(Cur_pre_BB == pre_BB){
@@ -47,7 +47,7 @@ PreservedAnalyses PhierasePass::run(Function &F, FunctionAnalysisManager &FAM){
               ReplaceInstWithValue(Cloned_BB->getInstList(), tmp_itr, CurVal); //replace original instruction to modfied value.
             }
           }
-        } else {
+        } else {//else replace operands to new value
           for(auto OI = I->op_begin(), OE = I->op_end(); OI != OE; ++OI){
             Value *CurVal = *OI;
             Value *CurVal2 = VM[CurVal];
@@ -58,7 +58,7 @@ PreservedAnalyses PhierasePass::run(Function &F, FunctionAnalysisManager &FAM){
         }
       }
 
-      Cloned_BB->insertInto(&F,Cur_BB);
+      Cloned_BB->insertInto(&F,Cur_BB);//insert cloneed block to function
 
       Instruction *pre_terminator = pre_BB->getTerminator();
       pre_terminator->replaceSuccessorWith(Cur_BB,Cloned_BB);
