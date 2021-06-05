2a050145f258fe598674c97f7804bfbeb19ed109
CookieHCl <nhc7502@snu.ac.kr>
2021-06-06 04:27:22 +0900

    Refactor code

--- src/core/MemoryToStackPass.cpp
+++ src/core/MemoryToStackPass.cpp
@@ -5,111 +5,127 @@ const char* MemoryToStackPass::NEW_MALLOC_NAME = "____malloc";
 const char* MemoryToStackPass::NEW_FREE_NAME = "____free";
 const char* MemoryToStackPass::STACK_ALLOC_NAME = "____stackalloc";
 
-#define CREATE_FUNCTION(name, type, M) \
-  Function::Create(type, Function::ExternalLinkage, name, M)
-
-void MemoryToStackPass::replaceMalloc(Module &M, Value* StackCounter) {
-  logs() << "Replacing malloc...\n";
-  Function* OrigMalloc = M.getFunction("malloc");
+/*
+// Note that in llvm, global variable is pointer
+____malloc(size) {
+  if (size + *StackCounter > STACK_SIZE) { // CondBB
+    return malloc(size); // MallocBB
+  }
+  else {
+    *StackCounter += size;
+    return ____stackalloc(size); // StackBB
+  }
+}
+*/
+Function* MemoryToStackPass::createNewMalloc(Module &M, Function* OrigMalloc,
+    Value* StackCounter, IntegerType* I64Ty) {
   if (!OrigMalloc) {
-    logs() << "no malloc\n";
-    return;
+    llvm_unreachable("There must be malloc to use stack");
   }
 
   FunctionType* MallocType = OrigMalloc->getFunctionType();
-  Function* NewMalloc = CREATE_FUNCTION(NEW_MALLOC_NAME, MallocType, M);
-  Function* StackAlloc = CREATE_FUNCTION(STACK_ALLOC_NAME, MallocType, M);
-
-  /*
-  // Note that global variable is pointer
-  ____malloc(size) {
-    if (size + *StackCounter > STACK_SIZE) { // CondBB
-      return malloc(size); // MallocBB
-    }
-    else {
-      *StackCounter += size;
-      return ____stackalloc(size); // StackBB
-    }
-  }
-  */
+  Function* NewMalloc = Function::Create(MallocType, Function::ExternalLinkage,
+      NEW_MALLOC_NAME, M);
+  Function* StackAlloc = Function::Create(MallocType, Function::ExternalLinkage,
+      STACK_ALLOC_NAME, M);
 
   BasicBlock* CondBB = BasicBlock::Create(M.getContext(), Twine(), NewMalloc);
   BasicBlock* MallocBB = BasicBlock::Create(M.getContext(), Twine(), NewMalloc);
   BasicBlock* StackBB = BasicBlock::Create(M.getContext(), Twine(), NewMalloc);
 
   // Instructions for CondBB
-  auto* OldSize = new LoadInst(Type::getInt64Ty(M.getContext()),
-      StackCounter, Twine(), CondBB);
+  auto* OldSize = new LoadInst(I64Ty, StackCounter, Twine(), CondBB);
   auto* NewSize = BinaryOperator::Create(BinaryOperator::Add,
       NewMalloc->getArg(0), OldSize, Twine(), CondBB);
   auto* HasOverflow = new ICmpInst(*CondBB, ICmpInst::ICMP_SGT,
-      NewSize, ConstantInt::getSigned(Type::getInt64Ty(M.getContext()), STACK_SIZE));
+      NewSize, ConstantInt::getSigned(I64Ty, STACK_SIZE));
   BranchInst::Create(MallocBB, StackBB, HasOverflow, CondBB);
 
   // Instructions for MallocBB
-  auto* CallMalloc= CallInst::Create(MallocType, OrigMalloc,
+  auto* CallMalloc = CallInst::Create(MallocType, OrigMalloc,
       NewMalloc->getArg(0), Twine(), MallocBB);
   ReturnInst::Create(M.getContext(), CallMalloc, MallocBB);
 
   // Instructions for StackBB
   new StoreInst(NewSize, StackCounter, StackBB);
-  auto* CallStackAlloc= CallInst::Create(MallocType, StackAlloc,
+  auto* CallStackAlloc = CallInst::Create(MallocType, StackAlloc,
       NewMalloc->getArg(0), Twine(), StackBB);
   ReturnInst::Create(M.getContext(), CallStackAlloc, StackBB);
+
+  return NewMalloc;
 }
 
-void MemoryToStackPass::replaceFree(Module &M) {
-  logs() << "Replacing free...\n";
-  Function* OrigFree = M.getFunction("free");
+/*
+// Note that pointer is in heap if pointer >= 204800
+____free(p) {
+  if (p > 123456) { // CondBB
+    free(p); // FreeBB
+  }
+  else {
+    return; // VoidBB
+  }
+}
+*/
+Function* MemoryToStackPass::createNewFree(Module &M, Function* OrigFree,
+    IntegerType* I64Ty) {
   if (!OrigFree) {
-    logs() << "no free\n";
-    return;
+    return nullptr;
   }
 
   FunctionType* FreeType = OrigFree->getFunctionType();
-  Function* NewFree = CREATE_FUNCTION(NEW_FREE_NAME, FreeType, M);
-
-  /*
-  // Note that pointer is in heap if pointer >= 204800
-  ____free(p) {
-    if (p > 123456) { // CondBB
-      free(p); // FreeBB
-    }
-    else {
-      return; // VoidBB
-    }
-  }
-  */
+  Function* NewFree = Function::Create(FreeType, Function::ExternalLinkage,
+      NEW_FREE_NAME, M);
 
   BasicBlock* CondBB = BasicBlock::Create(M.getContext(), Twine(), NewFree);
   BasicBlock* FreeBB = BasicBlock::Create(M.getContext(), Twine(), NewFree);
   BasicBlock* VoidBB = BasicBlock::Create(M.getContext(), Twine(), NewFree);
 
   // Instructions for CondBB
-  auto* ConstantPtr = ConstantExpr::getIntToPtr(ConstantInt::get(Type::getInt64Ty(M.getContext()), 123456),
+  auto* ConstantPtr = ConstantExpr::getIntToPtr(ConstantInt::get(I64Ty, 123456),
       NewFree->getArg(0)->getType());
   auto* IsHeap = new ICmpInst(*CondBB, ICmpInst::ICMP_UGT,
       NewFree->getArg(0), ConstantPtr);
   BranchInst::Create(FreeBB, VoidBB, IsHeap, CondBB);
 
   // Instructions for FreeBB
-  auto* CallMalloc= CallInst::Create(FreeType, OrigFree,
-      NewFree->getArg(0), Twine(), FreeBB);
+  auto* CallMalloc = CallInst::Create(FreeType, OrigFree, NewFree->getArg(0),
+      Twine(), FreeBB);
   ReturnInst::Create(M.getContext(), FreeBB);
 
   // Instructions for VoidBB
   ReturnInst::Create(M.getContext(), VoidBB);
+
+  return NewFree;
 }
 
 PreservedAnalyses MemoryToStackPass::run(Module &M, ModuleAnalysisManager &MAM) {
   logs() << "---------- Start MemoryToStackPass ----------\n";
 
-  auto* ConstZero = ConstantInt::getSigned(Type::getInt64Ty(M.getContext()), 0);
-  auto* StackCounter = new GlobalVariable(M, Type::getInt64Ty(M.getContext()),
-      false, GlobalVariable::PrivateLinkage, ConstZero, STACK_COUNTER_NAME);
+  IntegerType* I64Ty = Type::getInt64Ty(M.getContext());
 
-  replaceMalloc(M, StackCounter);
-  replaceFree(M);
+  Function* OrigMalloc = M.getFunction("malloc");
+  // If we don't have malloc, we don't need to change to stack at all
+  if (!OrigMalloc) {
+    logs() << "Module doesn't have malloc;\n"
+        << "---------- End MemoryToStackPass ----------";
+    return PreservedAnalyses::all();
+  }
+
+  // It's normal to not have free; don't check it
+  Function* OrigFree = M.getFunction("free");
+
+  // global variable to count stack's size
+  auto* StackCounter = new GlobalVariable(M, I64Ty, false,
+      GlobalVariable::PrivateLinkage, ConstantInt::getNullValue(I64Ty),
+      STACK_COUNTER_NAME);
+
+  // create new malloc & free
+  Function* NewMalloc = createNewMalloc(M, OrigMalloc, StackCounter, I64Ty);
+  Function* NewFree = createNewFree(M, OrigFree, I64Ty);
+
+  // replace malloc & free
+  //replaceFunction(M, OrigMalloc, NewMalloc);
+  //replaceFunction(M, OrigFree, NewFree);
 
   logs() << "---------- End MemoryToStackPass ----------\n";
   return PreservedAnalyses::none();
--- src/core/MemoryToStackPass.h
+++ src/core/MemoryToStackPass.h
@@ -10,10 +10,10 @@ using namespace llvm;
 
 class MemoryToStackPass : public PassInfoMixin<MemoryToStackPass> {
 private:
-  static const char* STACK_COUNTER_NAME;
-  static const char* NEW_MALLOC_NAME;
-  static const char* NEW_FREE_NAME;
-  static const char* STACK_ALLOC_NAME;
+  static const char* STACK_COUNTER_NAME; // "____stacksize"
+  static const char* NEW_MALLOC_NAME; // "____malloc"
+  static const char* NEW_FREE_NAME; // "____free"
+  static const char* STACK_ALLOC_NAME; // "____stackalloc"
   static const int64_t STACK_SIZE = 102400;
 
   const bool isVerbose;
@@ -22,8 +22,11 @@ private:
     return isVerbose ? outs() : nulls();
   }
 
-  void replaceMalloc(Module &M, Value* StackCounter);
-  void replaceFree(Module &M);
+  Function* createNewMalloc(Module &M, Function* OrigMalloc,
+      Value* StackCounter, IntegerType* Int64Ty);
+  Function* createNewFree(Module &M, Function* OrigFree, IntegerType* Int64Ty);
+
+  void replaceFunction(Module &M, Function* OrigFun, Function* NewFun);
 
 public:
   MemoryToStackPass(bool isVerbose = false) : isVerbose(isVerbose) {}
