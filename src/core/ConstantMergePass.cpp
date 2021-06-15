6723df243df25f977e38a7288b1121f8023bd4f9
mathamp <jwzz123123@gmail.com>
2021-06-15 23:20:41 +0900

    feat : Implement ChainUnit based merging
    Implement ChainUnit class which supports ChainUnit operations.
    ChainUnit manage inst<->ChainUnit, (ChainUnit, ChainUnit)->ChainUnit.
    To implement more, only reinforce ChainUnit.

--- src/core/ConstantMergePass.cpp
+++ src/core/ConstantMergePass.cpp
@@ -1,26 +1,123 @@
 #include "ConstantMergePass.h"
 
-ConstantMergePass::ChainUnit ConstantMergePass::getChainUnit(Instruction *inst) {
+/*
+ *                        Constant Merge Pass
+ *
+ *  This Pass merge consecutive constant binary operation.
+ *  It regards typical binary operations like c = c + 1 or c = c * 2
+ *  This Pass is recommended to place immediately after LoopVectorize
+ *  or passes like merge multiple blocks contains typicall binary operations.
+ *  
+ */ 
 
-  if (!inst->getType()->isIntegerTy()) return {NULL, NULL};
+Instruction* ConstantMergePass::ChainUnit::createInstruction(Instruction *flagInst) {
+  Value *op1 = value;
+  Value *op2 = constant;
+  if (this->isReversed) std::swap(op1, op2);
+  return BinaryOperator::CreateWithCopiedFlags(this->binOp, op1, op2, flagInst);
+}
 
+ConstantMergePass::ChainUnit ConstantMergePass::ChainUnit::merge(ConstantMergePass::ChainUnit &sourceUnit) {
+  if (this->constant->getType() != sourceUnit.constant->getType()) return ChainUnit::blank();
+
+  if (this->binOp != sourceUnit.binOp) return ChainUnit::blank();
+
+  APInt sourceInt = sourceUnit.constant->getValue();
+  APInt Int = this->constant->getValue();
+
+  switch (this->binOp) {
+    case Instruction::Sub: {
+      // Modulo sub is distributive so do not have to care about overflow/underflow
+      // There are four cases.
+      //
+      // Case 1.
+      // x1 = x0 - c1
+      // x2 = x1 - c2
+      // => x2 = x0 - c1 - c2 = x0 - (c1 + c2)
+      //
+      // Case 2.
+      // x1 = x0 - c1
+      // x2 = c2 - x1
+      // => x2 = c2 - (x0 - c1) = (c1 + c2) - x0
+      //
+      // Case 3.
+      // x1 = c1 - x0
+      // x2 = x1 - c2
+      // => x2 = c1 - x0 - c2 = (c1 - c2) - x0
+      //
+      // Case 4.
+      // x1 = c1 - x0
+      // x2 = c2 - x1
+      // => x2 = c2 - c1 + x0 = x0 - (c1 - c2)
+      //
+      // Therefore, each isReversed determines const and isReversed of mergeUnit
+      //
+      // if x1 = Value - Constant, the result constant is c1 + c2
+      // else, the result consttant is c1 - c2
+      //
+      // if (x2 = V - C and x1 = V - C) or (x2 = C - V and x1 = C - V), the result is Value - Constant
+      // else, the result is Constant - Value
+      //
+      APInt newInt = sourceUnit.isReversed ? (sourceInt - Int) : (sourceInt + Int);
+      if (sourceUnit.isReversed) outs() << sourceInt << " - " << Int << " = " << newInt << "\n";
+      Constant *newConst = ConstantInt::get(this->constant->getType(), newInt);
+      ConstantInt *newConstInt = dyn_cast<ConstantInt>(newConst);
+      bool newIsReversed = this->isReversed ^ sourceUnit.isReversed;
+      return newConstInt ? ChainUnit(this->binOp, sourceUnit.value, newConstInt, newIsReversed) : ChainUnit::blank();
+    }
+    case Instruction::Mul:
+    case Instruction::Add:
+    case Instruction::Shl:
+    case Instruction::AShr:
+    case Instruction::LShr: {
+      APInt newInt = (this->binOp == Instruction::Mul) ? (sourceInt * Int) : (sourceInt + Int);
+      Constant *newConst = ConstantInt::get(this->constant->getType(), newInt);
+      ConstantInt *newConstInt = dyn_cast<ConstantInt>(newConst);
+      return newConstInt ? ChainUnit(this->binOp, sourceUnit.value, newConstInt, false) : ChainUnit::blank();
+    }
+    default: 
+      return ChainUnit::blank();
+  }
+}
+
+ConstantMergePass::ChainUnit ConstantMergePass::ChainUnit::createChainUnit(Instruction *inst) {
+  if (!inst->getType()->isIntegerTy()) return ChainUnit::blank();
+
+  BinaryOperator *binOperator = dyn_cast<BinaryOperator>(inst);
+
+  if (!binOperator) return ChainUnit::blank();
+
+  BinaryOperator::BinaryOps binOp = binOperator->getOpcode();
   Value *val;
   ConstantInt *con;
-  const bool isCommutative = Instruction::isCommutative(inst->getOpcode());
 
-  if (match(inst, m_c_BinOp(m_Value(val), m_ConstantInt(con)))) return {val, con};
+  switch (inst->getOpcode()) {
+    case Instruction::Mul:
+    case Instruction::Add:
+    case Instruction::Sub: {
+      if (match(inst, m_BinOp(m_Value(val), m_ConstantInt(con)))) return {binOp, val, con, false};
+      if (match(inst, m_BinOp(m_ConstantInt(con), m_Value(val)))) return {binOp, val, con, true};
+    }
+    case Instruction::Shl:
+    case Instruction::AShr:
+    case Instruction::LShr:
+      if (match(inst, m_BinOp(m_Value(val), m_ConstantInt(con)))) return {binOp, val, con, false};
+    default:
+      return ChainUnit::blank();
+  }
   
-  if (isCommutative && match(inst, m_c_BinOp(m_ConstantInt(con), m_Value(val)))) return {val, con};
-  
-  return {NULL, NULL};
+  return ChainUnit::blank();
 }
 
 bool ConstantMergePass::tryMergeConstant(Instruction *inst, BinaryOperator::BinaryOps ops) {
   bool isChanged = false;
 
-  ChainUnit chainUnit = getChainUnit(inst);
+  logs() << "Target : " << *inst << "\n";
 
-  if (!chainUnit.first) return isChanged;
+  ChainUnit chainUnit = ChainUnit::createChainUnit(inst);
+  if (!chainUnit.isValid()) return isChanged;
+
+  logs() << chainUnit << "\n";
 
   SmallVector<User*> userVector(inst->user_begin(), inst->user_end());
 
@@ -30,20 +127,17 @@ bool ConstantMergePass::tryMergeConstant(Instruction *inst, BinaryOperator::Bina
 
     if (userInst->getOpcode() != ops) continue;
 
-    ChainUnit nextChainUnit = getChainUnit(userInst);
-    if (!nextChainUnit.first) continue;
+    ChainUnit nextChainUnit = ChainUnit::createChainUnit(userInst);
+    logs() << "next : " << nextChainUnit << "\n";
+    if (!nextChainUnit.isValid()) continue;
 
-    if (nextChainUnit.second->getType() != chainUnit.second->getType()) continue;
+    ChainUnit newChainUnit = nextChainUnit.merge(chainUnit);
+    logs() << "new : " << newChainUnit << "\n";
+    if (!newChainUnit.isValid()) continue;
 
-    APInt currentInt = chainUnit.second->getValue();
-    APInt nextInt = nextChainUnit.second->getValue();
-
-    APInt newInt = (ops == Instruction::Mul) ? (currentInt * nextInt) : (currentInt + nextInt);
-
-    Constant *newConst = ConstantInt::get(nextChainUnit.second->getType(), newInt);
-
-    Instruction *newInst = BinaryOperator::CreateWithCopiedFlags(ops, chainUnit.first, newConst, userInst);
+    Instruction *newInst = newChainUnit.createInstruction(userInst);
     ReplaceInstWithInst(userInst, newInst);
+    logs() << "new inst : " << *newInst << "\n";
 
     isChanged = true;
   }
@@ -77,10 +171,7 @@ PreservedAnalyses ConstantMergePass::run(Function &F, FunctionAnalysisManager &F
 
   bool isChanged = false;
 
-  vector<BinaryOperator::BinaryOps> opsVector = {Instruction::Add, Instruction::Mul, Instruction::Sub,
-                                                 Instruction::Shl, Instruction::LShr, Instruction::AShr};
-  
-  for (BinaryOperator::BinaryOps ops : opsVector)
+  for (BinaryOperator::BinaryOps ops : ChainUnit::getAvailableOps())
     isChanged |= this->MergeConstant(F, ops);
 
   logs() << "[ConstantMergePass] @" << F.getName() << " END\n";
--- src/core/ConstantMergePass.h
+++ src/core/ConstantMergePass.h
@@ -12,11 +12,32 @@ using namespace llvm::PatternMatch;
 
 class ConstantMergePass : public llvm::PassInfoMixin<ConstantMergePass> {
 public:
-  using ChainUnit = std::pair<Value*, ConstantInt*>;
+class ChainUnit {
+public:
+  BinaryOperator::BinaryOps binOp;
+  Value *value;
+  ConstantInt *constant;
+  bool isReversed;
+  bool isValid() { return this->value != NULL; }
+  ChainUnit(BinaryOperator::BinaryOps binOp, Value *v, ConstantInt* c, 
+            bool isReversed) : binOp(binOp), value(v), constant(c), isReversed(isReversed) {};
+  static ChainUnit createChainUnit(Instruction *inst);
+  static ChainUnit blank() { return {Instruction::Add, NULL, NULL, false}; }
+  static vector<BinaryOperator::BinaryOps> getAvailableOps(){
+    return {Instruction::Add, Instruction::Mul, Instruction::Sub, Instruction::Shl, Instruction::LShr, Instruction::AShr};
+  }
+  Instruction* createInstruction(Instruction *flagInst);
+  ChainUnit merge(ChainUnit &sourceUnit);
+  friend raw_ostream& operator << (raw_ostream& os, ChainUnit &unit) {
+    if (unit.isValid()) 
+      return os << "value : " << *unit.value << " / const : " << *unit.constant 
+                << (unit.isReversed ? " (Reversed order)" : " (In order)");
+    else return os << "INVALID ChainUnit";
+  }
+};
 private:
   bool isVerbose = false;
   raw_ostream& logs() const { return isVerbose ? outs() : nulls(); }
-  ChainUnit getChainUnit(Instruction *inst);
   bool MergeConstant(Function &F, BinaryOperator::BinaryOps ops);
   bool tryMergeConstant(Instruction *inst, BinaryOperator::BinaryOps ops);
 public:
