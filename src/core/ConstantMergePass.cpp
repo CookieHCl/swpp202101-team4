aab38ef7d726da044e8e55ff487b959a4fcce4e5
mathamp <jwzz123123@gmail.com>
2021-06-15 15:00:04 +0900

    feat : Implement nested loop based mergeConstant

--- src/core/ConstantMergePass.cpp
+++ src/core/ConstantMergePass.cpp
@@ -1,6 +1,89 @@
 #include "ConstantMergePass.h"
 
-PreservedAnalyses ConstantMergePass::run(Function &F, FunctionAnalysisManager &FAM) {
+ConstantMergePass::ChainUnit ConstantMergePass::getChainUnit(Instruction *inst) {
 
-  return PreservedAnalyses::none();
+  if (!inst->getType()->isIntegerTy()) return {NULL, NULL};
+
+  Value *val;
+  ConstantInt *con;
+  const bool isCommutative = Instruction::isCommutative(inst->getOpcode());
+
+  if (match(inst, m_c_BinOp(m_Value(val), m_ConstantInt(con)))) return {val, con};
+  
+  if (isCommutative && match(inst, m_c_BinOp(m_ConstantInt(con), m_Value(val)))) return {val, con};
+  
+  return {NULL, NULL};
+}
+
+bool ConstantMergePass::tryMergeConstant(Instruction *inst, BinaryOperator::BinaryOps ops) {
+  bool isChanged = false;
+
+  ChainUnit chainUnit = getChainUnit(inst);
+
+  if (!chainUnit.first) return isChanged;
+
+  SmallVector<User*> userVector(inst->user_begin(), inst->user_end());
+
+  for (User *user : userVector) {
+    Instruction *userInst = dyn_cast<Instruction>(user);
+    if (!userInst) continue;
+
+    if (userInst->getOpcode() != ops) continue;
+
+    ChainUnit nextChainUnit = getChainUnit(userInst);
+    if (!nextChainUnit.first) continue;
+
+    if (nextChainUnit.second->getType() != chainUnit.second->getType()) continue;
+
+    APInt currentInt = chainUnit.second->getValue();
+    APInt nextInt = nextChainUnit.second->getValue();
+
+    APInt newInt = (ops == Instruction::Mul) ? (currentInt * nextInt) : (currentInt + nextInt);
+
+    Constant *newConst = ConstantInt::get(nextChainUnit.second->getType(), newInt);
+
+    Instruction *newInst = BinaryOperator::CreateWithCopiedFlags(ops, chainUnit.first, newConst, userInst);
+    ReplaceInstWithInst(userInst, newInst);
+
+    isChanged = true;
+  }
+
+  return isChanged;
+}
+
+bool ConstantMergePass::MergeConstant(Function &F, BinaryOperator::BinaryOps ops) {
+  bool isInstChanged = false;
+
+  const bool isCommutative = Instruction::isCommutative(ops);
+
+  while (true) {
+    bool isChanged = false;
+    for (inst_iterator I = inst_begin(F), E = inst_end(F); I != E; ++I) {
+      Instruction *inst = &*I;
+      if (inst->getType()->isIntegerTy() && inst->getOpcode() == ops && inst->getNumOperands() == 2)
+        if ((isChanged = tryMergeConstant(inst, ops))) break;
+    }
+
+    isInstChanged |= isChanged;
+    if (!isChanged) break;
+  }
+
+  return isInstChanged;
+}
+
+PreservedAnalyses ConstantMergePass::run(Function &F, FunctionAnalysisManager &FAM) {
+  logs() << "[ConstantMergePass] @" << F.getName() << '\n';
+  makeAllocaAsPHI(F, FAM);
+
+  bool isChanged = false;
+
+  vector<BinaryOperator::BinaryOps> opsVector = {Instruction::Add, Instruction::Mul, Instruction::Sub,
+                                                 Instruction::Shl, Instruction::LShr, Instruction::AShr};
+  
+  for (BinaryOperator::BinaryOps ops : opsVector)
+    isChanged |= this->MergeConstant(F, ops);
+
+  logs() << "[ConstantMergePass] @" << F.getName() << " END\n";
+
+  return isChanged ? PreservedAnalyses::none() : PreservedAnalyses::all();
 }
--- src/core/ConstantMergePass.h
+++ src/core/ConstantMergePass.h
@@ -2,16 +2,25 @@
 #define CONSTANTMERGEPASS_H
 
 #include "LoopSimplifyUtil.h"
+#include "llvm/IR/PatternMatch.h"
 
 #include <vector>
 
 using namespace llvm;
 using namespace std;
+using namespace llvm::PatternMatch;
 
 class ConstantMergePass : public llvm::PassInfoMixin<ConstantMergePass> {
-  bool isVerbose = false;
 public:
-  ConstantMergePass::ConstantMergePass(bool isVerbose = false) : isVerbose(isVerbose) {};
+  using ChainUnit = std::pair<Value*, ConstantInt*>;
+private:
+  bool isVerbose = false;
+  raw_ostream& logs() const { return isVerbose ? outs() : nulls(); }
+  ChainUnit getChainUnit(Instruction *inst);
+  bool MergeConstant(Function &F, BinaryOperator::BinaryOps ops);
+  bool tryMergeConstant(Instruction *inst, BinaryOperator::BinaryOps ops);
+public:
+  ConstantMergePass(bool isVerbose = false) : isVerbose(isVerbose) {};
   PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM);
 };
 
